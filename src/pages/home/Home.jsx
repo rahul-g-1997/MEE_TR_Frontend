431bf513ed553f921cd951b3c22882a94dc4fd22
Rahul Gaydhane <rahulgaydhane7@gmail.com>
2024-03-28 15:24:19 +0530

    senting up home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,10 @@ import { Signin } from "../../components/index";
 
 export default function Home() {
   return (
-    <Container maxWidth="md" sx={{ marginTop: 3 }}>
+    <Container maxWidth="lg" sx={{ marginTop: 3 }}>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          
+          ok
         </Grid>
         <Grid item xs={12} md={6}>
           <Signin />
